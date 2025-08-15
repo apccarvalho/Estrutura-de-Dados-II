0c88f09b7c480d60cfe2f0df190afa8f4e338707
apccarvalho <andreiacarvalho.apc@gmail.com>
2025-08-15 13:20:51 -0300

    atividade 02 da aula 02 - selectionsort com flag

--- Atividades/Aula02-atividade02.py
+++ Atividades/Aula02-atividade02.py
@@ -1,8 +1,7 @@
 import time
 
-print("Selectionsort com numeros ordenados:")
+print("Antes do Selectionsort numeros ordenados:")
 vetor = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-print("Antes do Selectionsort:")
 print(vetor)
     
 print("--------------")
@@ -10,15 +9,22 @@ print("--------------")
 print("Ordenando o vetor.....")
 
 inicio = time.perf_counter()
+
 for i in range(len(vetor)-1):
     menor = vetor[i]
     posicao = i
+    flag = False
     for j in range(i+1, len(vetor)):
         if(vetor[j] < menor):
             menor = vetor[j] 
-            posicao = j;    
+            posicao = j;   
+            flag = True 
     if posicao != i:
-        vetor[posicao], vetor[i] = vetor[i], menor  
+        vetor[posicao], vetor[i] = vetor[i], menor
+        flag = True  
+    if not flag:
+        break
+            
 fim = time.perf_counter()
 tempoExecucao = fim-inicio
 
@@ -27,9 +33,12 @@ print(vetor)
 print(f"Tempo de execucao: {tempoExecucao:.6f} segundos")
 print("--------------")
 
-print("Selectionsort com numeros nao ordenados:")
+
+
+
+
+print("Antes do Selectionsort numeros nao ordenados:")
 vetor = [10,20,15,30,5,1,100,40,86,201,4,106,31,9,16,199,500,3,47,55]
-print("Antes do Selectionsort:")
 print(vetor)
     
 print("--------------")
@@ -40,12 +49,18 @@ inicio = time.perf_counter()
 for i in range(len(vetor)-1):
     menor = vetor[i]
     posicao = i
+    flag = False
     for j in range(i+1, len(vetor)):
         if(vetor[j] < menor):
             menor = vetor[j] 
-            posicao = j;    
+            posicao = j;   
+            flag = True 
     if posicao != i:
-        vetor[posicao], vetor[i] = vetor[i], menor  
+        vetor[posicao], vetor[i] = vetor[i], menor
+        flag = True  
+    if not flag:
+        break
+    
 fim = time.perf_counter()
 tempoExecucao = fim-inicio
 
