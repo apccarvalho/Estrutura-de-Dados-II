372129cde4f12440fb86eb3a4db0bb57888d7c57
apccarvalho <andreiacarvalho.apc@gmail.com>
2025-08-25 10:26:38 -0300

    Atividade 05

---
+++ Atividades/Atividade05part2.py
@@ -0,0 +1,78 @@
+import time
+import random
+
+def definirVetor(tamanho):
+    """
+    Função que preenche o vetor com número aleatórios, que recebe como parâmetro o tamanho que será o vetor.
+    """
+    tamanhoVetor = tamanho
+    vetor = []
+    limiteInferior = 1
+    limiteSuperior = 200
+
+    for i in range(tamanhoVetor):
+        numero = random.randint(limiteInferior,limiteSuperior)
+        vetor.append(numero)
+    return vetor
+
+def insertionSort(novoVetor):
+    """
+    Insertion Sort otimizado que para se detectar que já está ordenado.
+    """
+    n = len(novoVetor)
+    
+    for i in range(1, n):
+        chave = novoVetor[i]
+        
+        if chave >= novoVetor[i - 1]: #Pula para próxima iteração
+            continue
+        
+        j = i - 1
+        while j >= 0 and novoVetor[j] > chave:
+            novoVetor[j + 1] = novoVetor[j]
+            j -= 1
+        
+        novoVetor[j + 1] = chave
+    
+    return novoVetor
+
+vetor = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+print("Antes do insertionSort com numeros ja ordenados:")
+print(vetor)
+    
+print("----------------------------------------------------------")
+
+print("Ordenando o vetor.....")
+inicio = time.perf_counter()
+
+insertionSort(vetor)
+
+fim = time.perf_counter()
+tempoExecucao = fim-inicio
+
+print("Apos o insertionSort:")
+print(vetor)
+print(f"Tempo de execucao: {tempoExecucao:.6f} segundos")
+print("----------------------------------------------------------")
+
+print("Nova interacao:")
+vetor = definirVetor(20)
+
+print("Antes do insertionSort com numeros nao ordenados:")
+print(vetor)
+    
+print("-----------------------------------------------------------")
+
+print("Ordenando o vetor.....")
+
+inicio = time.perf_counter()
+insertionSort(vetor)
+
+fim = time.perf_counter()
+tempoExecucao = fim-inicio
+
+print("Apos o insertion sort:")
+print(vetor)
+print(f"Tempo de execucao: {tempoExecucao:.6f} segundos")
+print("-----------------------------------------------------------")
